21137337e76605de1fd2f8bb17cdec9bfc8ea657
Matheus <mporto1994@gmail.com>
2022-05-02 16:21:26 -0300

    all finly done

--- crud.sql
+++ crud.sql
@@ -4,31 +4,80 @@
 -- Criação
 
 -- 1)
+    INSERT INTO clientes
+  (nome, lealdade)
+  VALUES
+	('Georgia',0);
 
 
 
 -- 2)
 
+INSERT INTO pedidos
+  (status, cliente_id)
+  VALUES
+	('Recebido',6);
 
 
 -- 3)
 
-
+INSERT INTO produtos_pedidos
+  (pedido_id, produto_id)
+  VALUES
+	(3,1);
+INSERT INTO produtos_pedidos
+  (pedido_id, produto_id)
+  VALUES
+	(3,2);
+INSERT INTO produtos_pedidos
+  (pedido_id, produto_id)
+  VALUES
+	(3,6);
+INSERT INTO produtos_pedidos
+  (pedido_id, produto_id)
+  VALUES
+	(3,8);
+INSERT INTO produtos_pedidos
+  (pedido_id, produto_id)
+  VALUES
+	(3,8);
 
 -- Leitura
 
 -- 1)
+SELECT
+	c.id,
+    c.nome,
+    c.lealdade,
+	p.id,
+    p.status,
+    p.cliente_id,
+    pr.id,
+    pr.nome,
+    pr.tipo,
+    pr."preço",
+    pr.pts_de_lealdade
+FROM
+  produtos_pedidos pe
+JOIN pedidos p
+	ON pe.pedido_id = p.id
+JOIN produtos pr
+	ON pe.produto_id = pr.id
+JOIN clientes c
+	ON p.cliente_id = c.id
+WHERE c.nome LIKE 'Georgia';
 
 
 
 -- Atualização
 
 -- 1)
-
+UPDATE clientes SET lealdade = 48 WHERE clientes.nome LIKE 'Georgia';
 
 
 -- Deleção
 
 -- 1)
+DELETE FROM clientes WHERE clientes.nome LIKE 'Marcelo';
 
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,64 @@
 -- Seleções de dados
 
 -- 1)
-
+    SELECT
+	p.id,
+    p.status,
+    p.cliente_id,
+    pr.id,
+    pr.nome,
+    pr.tipo,
+    pr."preço",
+    pr.pts_de_lealdade
+FROM
+  produtos_pedidos pe
+JOIN pedidos p
+	ON pe.pedido_id = p.id
+JOIN produtos pr
+	ON pe.produto_id = pr.id;
 -- 2)
-
+    SELECT
+	pe.id
+FROM
+  produtos_pedidos pp
+JOIN pedidos pe
+	ON pp.pedido_id = pe.id
+JOIN produtos pr
+	ON pp.produto_id = pr.id
+    WHERE pr.nome LIKE 'Fritas';
 -- 3)
-
+SELECT
+	c.nome Gostam_de_Fritas
+FROM
+  produtos_pedidos pp
+JOIN pedidos pe
+	ON pp.pedido_id = pe.id
+JOIN produtos pr
+	ON pp.produto_id = pr.id
+JOIN clientes c
+	ON pe.cliente_id = c.id
+WHERE pr.nome LIKE 'Fritas';
 -- 4)
-
+SELECT
+	SUM(pr."preço")
+FROM
+  produtos_pedidos pp
+JOIN pedidos pe
+	ON pp.pedido_id = pe.id
+JOIN produtos pr
+	ON pp.produto_id = pr.id
+JOIN clientes c
+	ON pe.cliente_id = c.id
+WHERE c.nome LIKE 'Laura';
 -- 5)
+SELECT
+	pr.nome, 
+    COUNT(pr.nome)
+FROM
+  produtos_pedidos pp
+JOIN pedidos pe
+	ON pp.pedido_id = pe.id
+JOIN produtos pr
+	ON pp.produto_id = pr.id
+GROUP BY pr.nome
+	ORDER BY pr.nome;
